3cf05b1440743481acd87e88284c2fe925c0fa9e
Tyuiu.DyakovMS@outlook.com <Tyuiu.DyakovMS@outlook.com>
2024-09-14 10:34:41 +0500

    Task1

---
+++ Tyuiu.DyakovMS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Максим";
+        int age = 20;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
