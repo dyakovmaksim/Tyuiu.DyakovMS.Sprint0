7aa1cec3a2ad43233c2a7d2941050087747ae422
dyakovmaksim <pochta3478@mail.ru>
2024-09-19 12:43:00 +0500

    Task4

---
+++ Tyuiu.DyakovMS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DyakovMS.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.DyakovMS.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
